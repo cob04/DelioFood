875c8b36822daf80f24a9ef1c306fef366538292
Eric Ndung'u <ericobain@gmail.com>
2017-02-22 09:23:14 +0300

    updating the product image each the a product image is saved

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,33 @@
-from django.contrib import admin
+from copy import deepcopy
 
-# Register your models here.
+from django.contrib import admin
+from django.db.models import ImageField
+
+from mezzanine.core.admin import (DisplayableAdmin, TabularDynamicInlineAdmin)
+from mezzanine.pages.admin import PageAdmin
+
+from .forms import ImageWidget
+from .models import (Product, ProductImage, Variation)
+
+
+
+class ProductImageInline(TabularDynamicInlineAdmin):
+    model = ProductImage
+    formfield_overrides = {ImageField: {"widget": ImageWidget}}
+
+class VariationInline(TabularDynamicInlineAdmin):
+    model = Variation
+
+product_fieldsets = deepcopy(DisplayableAdmin.fieldsets)
+product_fieldsets[0][1]['fields'].insert(2, 'available')
+product_fieldsets[0][1]['fields'].extend(['content',])
+product_fieldsets = list(product_fieldsets)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['admin_thumb', 'title', 'price', 'available', 'status', 'admin_link']
+    list_display_links = ('admin_thumb', 'title')
+    list_filter = ['available', 'status', 'publish_date', 'expiry_date']
+    list_editable = ['available', 'status']
+    inlines = [ProductImageInline, VariationInline]
+    fieldsets = product_fieldsets
---
+++ shop/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.conf import settings
+from django.utils.safestring import mark_safe
+
+from mezzanine.core.templatetags.mezzanine_tags import thumbnail
+
+class ImageWidget(forms.FileInput):
+    """
+    Render a visible thumbnail for image fields.
+    """
+    def render(self, name, value, attrs):
+        rendered = super(ImageWidget, self).render(name, value, attrs)
+        if value:
+            orig = u"%s%s" % (settings.MEDIA_URL, value)
+            thumb = u"%s%s" % (settings.MEDIA_URL, thumbnail(value, 48, 48))
+            rendered = (u"<a target='_blank' href='%s'>"
+                        u"<img style='margin-right:6px;' src='%s'>"
+                        u"</a>%s" % (orig, thumb, rendered))
+        return mark_safe(rendered)
