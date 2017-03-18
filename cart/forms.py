31864486673da82317d774e9e3fba3dc30ed6f6f
Eric Ndung'u <ericobain@gmail.com>
2017-03-18 23:45:23 +0300

    product detail page redesign

--- cart/forms.py
+++ cart/forms.py
@@ -35,25 +35,20 @@ class CartAddProductForm(forms.Form):
         self.fields['instructions'].label="Additional information"
         self.helper = FormHelper()
         self.helper.form_id='cart_add'
-        self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-sm-4'
-        self.helper.field_class = 'col-sm-6'
+        #self.helper.form_class = 'form-horizontal'
         self.helper.layout = Layout(
             Div(
                 Field('variation'),
-                css_class='row',
             ),
             Div(
                 Field('quantity'),
-                css_class='row',
             ),
             Div(
                 Field('instructions',
                     placeholder="Indicate any addtional instructions and"
                         " dietary restrictions here.",
                     rows=4),
-                css_class="row",
             ),
-            Submit('Submit', 'Order Now', css_class='btn btn-lg btn-action'),
+            Submit('Submit', 'Order Now', css_class='btn btn-lg btn-action btn-block'),
         )
         self.helper.form_method = 'post'
