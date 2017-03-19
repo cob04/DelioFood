b6c4bea75c2291751ef08c6ba57d7d00290fd418
Eric Ndung'u <ericobain@gmail.com>
2017-03-19 03:37:53 +0300

    faq url, center images

--- faqs/admin.py
+++ faqs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FAQ
+
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ['question']
